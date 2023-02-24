f2ed6e15d0c3a7e53fb170a8dab727652b6ad31d
Hrćan Jan <xhrcan@stuba.sk>
2023-02-24 16:59:35 +0100

    feat: added function process

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ __license__ = "MIT"
 from fei.ppds import Thread
 from time import sleep
 
+def process():
+    print("here")
+
 # inspired from https://github.com/tj314/ppds-2023-cvicenia/blob/master/seminar2/04_ticket.py
 if __name__ == '__main__':
     DEFAULT_NUM_RUNS = 10
     NUM_THREADS = 5
+    threads = [Thread(process, i, DEFAULT_NUM_RUNS) for i in range(NUM_THREADS)]
+    [t.join() for t in threads]
     

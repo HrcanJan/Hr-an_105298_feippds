355d3fdef556e15f209a9c34b4be4028313aefde
Hrćan Jan <xhrcan@stuba.sk>
2023-02-24 16:58:29 +0100

    chore: imports and main

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ __author__ = "Jan Hrćan"
 __email__ = "xhrcan@stuba.sk"
 __license__ = "MIT"
 
+from fei.ppds import Thread
+from time import sleep
+
+# inspired from https://github.com/tj314/ppds-2023-cvicenia/blob/master/seminar2/04_ticket.py
+if __name__ == '__main__':
+    DEFAULT_NUM_RUNS = 10
+    NUM_THREADS = 5
+    

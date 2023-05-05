44cecccd5afa8fa3aa717683d97566b077014882
Hrćan Jan <xhrcan@stuba.sk>
2023-05-05 17:49:46 +0200

    feat: create main

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ __author__ = "Jan Hrćan"
 __email__ = "xhrcan@stuba.sk"
 __license__ = "MIT"
 
+from numba import cuda
+import matplotlib.pyplot as plt
 
-# inspired from https://github.com/tj314/ppds-2023-cvicenia/blob/master/seminar2/04_ticket.py
+
+def transform_to_grayscale(pixels):
+    return pixels
+
+
 if __name__ == '__main__':
-    print("y")
+    pixel = plt.imread('./img/sivy.jpg')
+    new_pixel = transform_to_grayscale(pixel)
+    plt.imsave('./img/gray.sivy.jpg', new_pixel, format="jpg")
+

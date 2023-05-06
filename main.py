ee99ca0f9f6d146f3c029bf383bc30252d664991
Hrćan Jan <xhrcan@stuba.sk>
2023-05-06 13:48:07 +0200

    docs: add descriptive comments

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """This module contains an implementation of CUDA.
-"""
+
+In this application, we use CUDA to demonstrate a difference in transforming images to grayscale using CPU and GPU
+separately."""
 
 __author__ = "Jan Hrćan"
 __email__ = "xhrcan@stuba.sk"
@@ -14,11 +16,23 @@ from math import ceil
 
 
 def transform_to_grayscale_cpu(pixels):
+    """This function transforms an image to grayscale using opencv's function.
+
+        parameters:
+        pixels:     an image array
+    """
     gray_img = cv2.cvtColor(pixels, cv2.COLOR_BGR2GRAY)
     return np.array(gray_img)
 
 
-def cpu(path):
+def cpu(path, save_path):
+    """This function loads all images from a given directory and applies grayscale to all images using CPU.
+        Then it saves the images in a given folder.
+
+        parameters:
+        path:       path to images folder
+        save_path:  path to where the images should be saved
+    """
     start_time = time.time()
     for filename in os.listdir(path):
         img_name = filename.split(".")
@@ -26,7 +40,7 @@ def cpu(path):
 
         img = cv2.imread(f'./img/{name}.{img_name[1]}')
         new_pixel = transform_to_grayscale_cpu(img)
-        cv2.imwrite(f'./gray_cpu/{name}.{img_name[1]}', new_pixel)
+        cv2.imwrite(f'./{save_path}/{name}.{img_name[1]}', new_pixel)
 
     end_time = time.time()
     timer = round(end_time - start_time, 2)
@@ -36,6 +50,12 @@ def cpu(path):
 # https://en.wikipedia.org/wiki/Grayscale#Converting_color_to_grayscale
 @cuda.jit
 def transform_to_grayscale(img, gray):
+    """This function transforms an image to grayscale using CUDA and GPU.
+
+        parameters:
+        img:     an image array
+        gray:    output variable of the grayscale image
+    """
     row, col = cuda.grid(2)
     if row >= img.shape[0] or col >= img.shape[1]:
         return
@@ -47,7 +67,14 @@ def transform_to_grayscale(img, gray):
     gray[row, col] = img_gray
 
 
-def gpu(path):
+def gpu(path, save_path):
+    """This function loads all images from a given directory and applies grayscale to all images using GPU.
+        Then it saves the images in a given folder.
+
+        parameters:
+        path:       path to images folder
+        save_path:  path to where the images should be saved
+    """
     start_time = time.time()
     for filename in os.listdir(path):
         img_name = filename.split(".")
@@ -62,13 +89,14 @@ def gpu(path):
         new_pixel = np.zeros((height, width))
         transform_to_grayscale[bpg, tpb](img, new_pixel)
 
-        cv2.imwrite(f'./gray/{name}.{img_name[1]}', new_pixel)
+        cv2.imwrite(f'./{save_path}/{name}.{img_name[1]}', new_pixel)
     end_time = time.time()
     timer = round(end_time - start_time, 2)
     print("GPU", timer)
 
 
 if __name__ == '__main__':
+    """Main"""
     directory = "./img"
-    cpu(directory)
-    gpu(directory)
+    cpu(directory, 'gray_cpu')
+    gpu(directory, 'gray')
